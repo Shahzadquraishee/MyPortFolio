0d28ca2fce3f8b6afad435432ae426e5f021dff6
shahzadquraishee <shahzadquraishee7@gmail.com>
2025-04-29 12:08:35 +0430

    Update project with latest changes

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/404.html',
-          dest: '' // root of dist
-        }
-      ]
-    })
   ]
 })
